c2a753ceb664bd974defaa7922e1acbf9b4008a3
Huelva91 <huelva91gmail.com>
2022-05-24 12:14:57 +0200

    Estilo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logoFcc from "./imagenes/freecodecamp-logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aplicacion-tareas">
+      <div className="fcc-logo-contenedor">
+        <img className="fcc-logo" src={logoFcc} alt="" />
+      </div>
     </div>
   );
 }
