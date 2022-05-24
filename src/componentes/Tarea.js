daeedfd58755dd6b0485d7bd2948c881756176a2
Huelva91 <huelva91gmail.com>
2022-05-24 13:41:14 +0200

    Condicional tarea completada

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -1,21 +1,16 @@
 import React from "react";
 import "../estilos/Tarea.css";
-import { icons } from "react-icons";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
-
-function Tarea({texto}){
-
-  return(
-    <div className="tarea-contenedor">
-      <div className="tarea-texto">
-        {texto}
-      </div>
-      <div className="tarea-icono">
-        Eliminar
+function Tarea({ texto, completada }) {
+  return (
+    <div className={completada ? "tarea-contenedor completada" : "tarea-contendor"}> 
+      <div className="tarea-texto">{texto}</div>
+      <div className="tarea-contenedor-iconos">
+        <AiOutlineCloseCircle className="tarea-icono" />
       </div>
     </div>
   );
-
 }
 
 export default Tarea;
