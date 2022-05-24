56478f43cffdc218e6921c506fa9003102ec9018
Huelva91 <huelva91gmail.com>
2022-05-24 13:29:35 +0200

    Add React icons

---
+++ src/componentes/Tarea.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../estilos/Tarea.css";
+import { icons } from "react-icons";
+
+
+function Tarea({texto}){
+
+  return(
+    <div className="tarea-contenedor">
+      <div className="tarea-texto">
+        {texto}
+      </div>
+      <div className="tarea-icono">
+        Eliminar
+      </div>
+    </div>
+  );
+
+}
+
+export default Tarea;
