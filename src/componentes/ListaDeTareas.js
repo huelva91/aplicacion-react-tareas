389b560656825ea63968eea292107df279112c8c
Huelva91 <huelva91gmail.com>
2022-05-25 16:46:19 +0200

    Completar Tarea

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -16,18 +16,38 @@ const agregarTarea = tarea => {//Recibe una tarea
     }
 }
 
+const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id); //Filtra las tareas que no sean iguales a la tarea que se quiere eliminar
+    setTareas(tareasActualizadas); //Actualiza el estado
+}
+
+const completarTarea = id => {
+    const tareasActualizadas = tareas.map(tarea => {
+        if (tarea.id === id) { //Si la tarea es igual a la que se quiere completar
+            tarea.completada = !tarea.completada; //Cambia el estado de la tarea
+        }
+        return tarea; //Devuelve la tarea
+    });
+    setTareas(tareasActualizadas); 
+}
+
+
+
 
     return (
         <>
         <TareaFormulario onSubmit={agregarTarea} />
         <div className="tareas-lista-contenedor">
             {
-                tareas.map((tarea) =>
+                tareas.map((tarea) =>//Recorre la lista de tareas
                 <Tarea 
                 key={tarea.id} //Para que no se repita el id
                 id={tarea.id} 
                 texto={tarea.texto} 
                 completada={tarea.completada}
+                completarTarea={completarTarea}
+                eliminarTarea={eliminarTarea} //Pasa la funcion eliminar al componente Tarea
+                
                 />
                 )
             }
